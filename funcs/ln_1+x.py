42dce5c8da91a2892422cb2b5cda038d1756bc49
GitHubSGN <free_register@163.com>
2024-04-22 11:19:42 +0800

    bug fix: ln(1+x) Taylor expand.

--- funcs/ln_1+x.py
+++ funcs/ln_1+x.py
@@ -10,8 +10,9 @@ def ln_1plusx():
     x = np.arange(-999,999) / 1000
     y1 = np.log( 1 + x )
     y2 = np.zeros_like(x)
-    for i in range(4):
-        y2 = y2 + np.power(x, i+1) / 2 * (1 if i % 2==0 else -1)
+    order_num = 30   # 100
+    for i in range(order_num):
+        y2 = y2 + np.power(x, i+1) / (i+1) * (1 if i % 2==0 else -1)
 
     line_plot(list(x), [list(y1), list(y2)], legend=["y1", "y2"], x_label='x', y_label='y',
               figure_name=f'y1 = Ln(1 + x) &. y2 = TSE of y1')
